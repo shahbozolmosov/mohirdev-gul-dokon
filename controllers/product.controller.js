ab361b5098eb8654d0a88cbbeacc81b26627d605
shahboz007 <olmasovshahboz007@gmail.com>
2024-06-25 21:15:15 +0500

    added add product page

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,12 +1,12 @@
-// Desc       Get dashboard home
-// Route      GET /dashboard
+// Desc       Get dashboard products page
+// Route      GET /dashboard/products
 // Access     Private
 const getProductsPage = async (req, res) => {
   try {
     const isAtuhenticated = req.session.isLogged;
     return res.render("dashboard/products/main", {
       title: "Products",
-      breadcrumb:[
+      breadcrumb: [
         {
           label: "Dashboard",
           route: "/dashboard",
@@ -14,10 +14,40 @@ const getProductsPage = async (req, res) => {
         {
           label: "Products",
           route: "/dashboard/products",
-          active: true
+          active: true,
         },
       ],
-      isAtuhenticated
+      isAtuhenticated,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Desc       Get dashboard products add page
+// Route      GET /dashboard/products/add
+// Access     Private
+const getProductsAddPage = async (req, res) => {
+  try {
+    const isAtuhenticated = req.session.isLogged;
+    return res.render("dashboard/products/add", {
+      title: "Product Add",
+      breadcrumb: [
+        {
+          label: "Dashboard",
+          route: "/dashboard",
+        },
+        {
+          label: "Products",
+          route: "/dashboard/products",
+        },
+        {
+          label: "Product Add",
+          route: "/dashboard/products/add",
+          active: true,
+        },
+      ],
+      isAtuhenticated,
     });
   } catch (error) {
     console.log(error);
@@ -26,4 +56,5 @@ const getProductsPage = async (req, res) => {
 
 module.exports = {
   getProductsPage,
+  getProductsAddPage,
 };
--- routes/dashboard.route.js
+++ routes/dashboard.route.js
@@ -2,12 +2,16 @@ const { Router } = require("express");
 const router = Router();
 const { getDashboardPage } = require("../controllers/dashboard.controller");
 const { protected } = require("../middlewares/auth");
-const { getProductsPage } = require("../controllers/product.controller");
+const {
+  getProductsPage,
+  getProductsAddPage,
+} = require("../controllers/product.controller");
 
 // Dashboard
 router.get("/", protected, getDashboardPage);
 
 // Products
 router.get("/products", protected, getProductsPage);
+router.get("/products/add", protected, getProductsAddPage);
 
 module.exports = router;

881d3708c25e372c82782aa124215dfc9d7bef6a
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-07-02 21:47:34 +0500

    added add region page

--- controllers/region.controller.js
+++ controllers/region.controller.js
@@ -1,8 +1,9 @@
+const { validationResult } = require("express-validator");
 const db = require("../models");
 const Region = db.region;
 
 // Desc       Get region page
-// Route      GET /dashboard/region
+// Route      GET /dashboard/regions
 // Access     Private
 const getRegionPage = async (req, res) => {
   try {
@@ -33,6 +34,56 @@ const getRegionPage = async (req, res) => {
   }
 };
 
+// Desc       Get add a new region page
+// Route      GET /dashboard/regions/add
+// Access     Private
+const getAddNewRegionPage = (req, res) => {
+  try {
+    // Authenticated
+    const isAuthenticated = req.session.isLogged;
+
+    return res.render("dashboard/region/add", {
+      title: "Add New Region",
+      isAuthenticated,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Desc       Add a new region
+// Route      POST /dashboard/regions
+// Access     Private
+const addNewRegion = (req, res) => {
+  try {
+    // Authenticated
+    const isAuthenticated = req.session.isLogged;
+
+    // Errors
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render("dashboard/region/main", {
+        title: "Regions",
+        breadcrumb: [
+          {
+            label: "Dashboard",
+            route: "/dashboard",
+          },
+          {
+            label: "Regions",
+            route: "/dashboard/regions",
+            active: true,
+          },
+        ],
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 module.exports = {
   getRegionPage,
+  getAddNewRegionPage
 };
--- routes/region.route.js
+++ routes/region.route.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
 const router = Router();
 const { protected } = require("../middlewares/auth");
-const { getRegionPage } = require("../controllers/region.controller");
+const {
+  getRegionPage,
+  getAddNewRegionPage,
+} = require("../controllers/region.controller");
 
 // Regions
 router.get("/", protected, getRegionPage);
+router.get("/add", protected, getAddNewRegionPage);
 
 module.exports = router;

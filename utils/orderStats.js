e4c03c4bf762bb836a1e03eccfee1def9aa04b2c
shahboz007 <olmasovshahboz007@gmail.com>
2024-07-30 21:10:47 +0500

    refactored of orderStats

--- utils/orderStats.js
+++ utils/orderStats.js
@@ -3,19 +3,9 @@ const db = require("../models");
 const Order = db.order;
 
 const calculateMonthlyOrders = async () => {
-  const currentMonth = new Date().getMonth() + 1;
-  const currentYear = new Date().getFullYear();
-
-  const firstDayOfCurrentMonth = new Date(currentYear, currentMonth - 1, 1);
-  const firstDayOfLastMonth = new Date(currentYear, currentMonth - 2, 1);
-  const lastDayOfLastMonth = new Date(
-    currentYear,
-    currentMonth - 1,
-    0,
-    23,
-    59,
-    59
-  );
+  // Monthly date
+  const { firstDayOfCurrentMonth, firstDayOfLastMonth, lastDayOfLastMonth } =
+    await getMonthlyDate();
 
   const ordersCurrentMonth = await Order.count({
     where: {

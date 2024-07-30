3dc52d7fc69a48029aef4d6e793cfa0beb152823
shahboz007 <olmasovshahboz007@gmail.com>
2024-07-30 20:53:41 +0500

    done orders calc

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -1,3 +1,4 @@
+const { Op } = require("sequelize");
 const db = require("../models");
 const Order = db.order;
 
@@ -8,20 +9,55 @@ const getDashboardPage = async (req, res) => {
   try {
     const isAuthenticated = req.session.isLogged;
 
-    // New Orders
-    const newOrders = await Order.findAll({
-      raw: true,
-      nest: true,
-      where: {
-        status: "new",
+    // New Orders  
+    const currentMonth = new Date().getMonth() + 1; // JavaScriptda oy 0 dan 11 gacha bo'ladi, shuning uchun +1
+    const currentYear = new Date().getFullYear();
+
+    const firstDayOfCurrentMonth = new Date(currentYear, currentMonth - 1, 1);
+    const firstDayOfLastMonth = new Date(currentYear, currentMonth - 2, 1);
+    const lastDayOfLastMonth = new Date(
+      currentYear,
+      currentMonth - 1,
+      0,
+      23,
+      59,
+      59
+    );
+
+    const ordersCurrentMonth = await Order.count({
+      where: { 
+        createdAt: {
+          [Op.gte]: firstDayOfCurrentMonth,
+        },
       },
     });
+
+    const ordersLastMonth = await Order.count({
+      where: { 
+        createdAt: {
+          [Op.gte]: firstDayOfLastMonth,
+          [Op.lte]: lastDayOfLastMonth,
+        },
+      },
+    });
+
+    let orderPercent;
+    if (ordersLastMonth === 0) {
+      orderPercent = ordersCurrentMonth === 0 ? 0 : 100;
+    } else {
+      orderPercent =
+      ((ordersCurrentMonth - ordersLastMonth) / ordersLastMonth) * 100;
+    } 
 
     return res.render("dashboard/home", {
       title: "Dashboard",
       isAuthenticated,
       cardsData: {
-        newOrderCount: newOrders.length,
+        newOrder: {
+          count: ordersCurrentMonth,
+          percent: Math.round(orderPercent*100)/100, // monthly
+          isPercentUp: orderPercent > 0,
+        },
       },
     });
   } catch (error) {

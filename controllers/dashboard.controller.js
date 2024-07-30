564043730274ea00799a903fb5ca4c482de2b25b
shahboz007 <olmasovshahboz007@gmail.com>
2024-07-30 20:00:15 +0500

    added orders count

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -1,12 +1,28 @@
+const db = require("../models");
+const Order = db.order;
+
 // Desc       Get dashboard home
 // Route      GET /dashboard
 // Access     Private
 const getDashboardPage = async (req, res) => {
   try {
     const isAuthenticated = req.session.isLogged;
+
+    // New Orders
+    const newOrders = await Order.findAll({
+      raw: true,
+      nest: true,
+      where: {
+        status: "new",
+      },
+    });
+
     return res.render("dashboard/home", {
       title: "Dashboard",
-      isAuthenticated
+      isAuthenticated,
+      cardsData: {
+        newOrderCount: newOrders.length,
+      },
     });
   } catch (error) {
     console.log(error);
